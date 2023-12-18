3e6d72bfcd3148fffeff967bd25ee9c9b8ec47f7
Nikhil Shinde <102649088+nikhilshinde5@users.noreply.github.com>
2023-12-18 10:30:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  var skills = ["HTML", "CSS", "JS", "React", "Node"];
+  var member = {
+    name: "John",
+    age: 23,
+    skills: skills,
+  };
+  // Write code here
+  var memberSkills = member.skills; // Change this line
+  // Only change code above this line
+  return memberSkills;
+}
